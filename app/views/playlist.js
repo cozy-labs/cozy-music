264af1100fe01f1077adfacccd56e19d8d2f24f5
Ysa <isabelle.yim@gmail.com>
2017-01-24 11:18:59 +0100

    Enhancement/play on dblclick playlist name (#134)
    * add play feature, on double click on a playlist

--- app/views/playlist.js
+++ app/views/playlist.js
@@ -3,7 +3,13 @@ import application from '../application'
 
 const PlaylistView = Mn.ItemView.extend({
 
+  template: require('./templates/playlist'),
+
   tagName: 'li',
+
+  events: {
+    'dblclick': 'playSelectedPlaylist'
+  },
 
   initialize (options) {
     this.template = options.template
@@ -13,6 +19,13 @@ const PlaylistView = Mn.ItemView.extend({
     let currentPlaylist = application.appState.get('currentPlaylist')
     let selected = currentPlaylist === this.model ? 'selected' : ''
     return 'playlist ' + selected
+  },
+
+  playSelectedPlaylist (e) {
+    let upNext = application.upNext.get('tracks')
+    application.channel.trigger('upnext:reset')
+    application.channel.trigger('upnext:addCurrentPlaylist')
+    application.appState.set('currentTrack', upNext.at(1))
   },
 
   modelEvents: { change: 'render' }
